cf12e208fe729f1a22246135707c2c73f619f83d
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-09-08 23:46:04 +0900

    Article#indexのviewが、コントローラから渡されたarticlesを参照し表示するように

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -8,25 +8,32 @@
 @section('content')
   <div class="container">
     <div class="card mt-3">
-      <div class="card-body d-flex flex-row">
-        <i class="fas fa-user-circle fa-3x mr-1"></i>
-        <div>
-          <div class="font-weight-bold">
-            ユーザー名
-          </div>
-          <div class="font-weight-lighter">
-            2020/2/1 12:00
+      {{-- Railsでいう articles.each do |article| --}}
+      @foreach($articles as $article)
+        <div class="card-body d-flex flex-row">
+          <i class="fas fa-user-circle fa-3x mr-1"></i>
+          <div>
+            <div class="font-weight-bold">
+              {{-- コントローラからbladeに渡された変数は{{ }} ←マスタッシュ で値を表示できる --}}
+              {{-- Railsでいう、article.user.name --}}
+              {{ $article->user->name }}
+            </div>
+            <div class="font-weight-lighter">
+              {{-- Railsでいう、article.created_at.strftime('%Y/%m/%d %H:%i') --}}
+              {{-- Laravelでは ->format で日付時刻のフォーマットを変更できる --}}
+              {{ $article->created_at->format('Y/m/d H:i') }}
+            </div>
           </div>
         </div>
-      </div>
-      <div class="card-body pt-0 pb-2">
-        <h3 class="h4 card-title">
-          記事タイトル
-        </h3>
-        <div class="card-text">
-          記事本文
+        <div class="card-body pt-0 pb-2">
+          <h3 class="h4 card-title">
+            {{ $article->title }}
+          </h3>
+          <div class="card-text">
+            {{ e($article->body) }}
+          </div>
         </div>
-      </div>
+      @endforeach
     </div>
   </div>
 @endsection
