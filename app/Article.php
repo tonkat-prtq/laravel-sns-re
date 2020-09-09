2032705c215bf2ce9766a4356ac3ba00b5483b95
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-09-09 21:05:55 +0900

    Add: Articleモデル作成、userと多対1のリレーションを組む

---
+++ app/Article.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+// Eloquent = モデルのこと
+// Eloquent ORM(Eloquent Object Relational Mapping)
+// データベースとモデルを関連付ける機能の名前
+
+use Illuminate\Database\Eloquent\Model;
+
+// 多対1のときBelongsTo
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Article extends Model
+{
+    // ArticleとUserは多(Articles)対1(user)
+    // :BelongsToで、userメソッドの戻り値の型宣言
+    // BelongsToクラス以外の型が帰ってきた場合はTypeErrorという例外が発生し処理が終了する
+    public function user(): BelongsTo 
+    {
+        // この$thisはArticleクラスのインスタンス自身
+        // この下のコードでは外部キー名の省略をしている
+        return $this->belongsTo('App\User'); 
+    }
+}
