0b0c6af79280123745f8905ecdbb7b1d02b5079c
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-09-10 21:48:55 +0900

    Add: コメント追加

--- resources/views/error_card_list.blade.php
+++ resources/views/error_card_list.blade.php
@@ -1,6 +1,9 @@
+{{-- $errors->any() でエラーの有無を返す --}}
 @if ($errors->any())
   <div class="card-text text-left alert alert-danger">
     <ul class="mb-0">
+      {{-- エラーメッセージが1件以上ある場合は、allメソッドで全エラーメッセージの配列を取得 -- }}
+      {{-- foreachで$errorに代入し、それを表示している --}}
       @foreach($errors->all() as $error)
         <li>{{ $error }}</li>
       @endforeach
