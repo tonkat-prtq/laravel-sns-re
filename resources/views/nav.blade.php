520b61b90d9ef5fe5855777d7f5e26ea901087d9
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-09-10 22:03:59 +0900

    Add: navbarにログインページへのリンクを貼る

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -14,7 +14,7 @@
 
     @guest
       <li class="nav-item">
-        <a class="nav-link" href="">ログイン</a>
+        <a class="nav-link" href="{{ route('login') }}">ログイン</a>
       </li>
     @endguest
 
