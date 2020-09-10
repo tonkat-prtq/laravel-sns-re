c1ed035030d50d842576e0ba3821d787c808e02d
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-09-10 21:41:35 +0900

    Add: navにログインやログアウトボタンを追加

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -4,38 +4,49 @@
 
   <ul class="navbar-nav ml-auto">
 
-    <li class="nav-item">
-      <a class="nav-link" href="">ユーザー登録</a>
-    </li>
+    {{-- bladeの認証ディレクティブという機能で、@guestから@endguestで囲まれている部分は未ログイン時のみ表示される --}}
+    {{-- また、@authから@endauthで囲まれている部分はログイン時のみ表示される --}}
+    @guest
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
+      </li>
+    @endguest
 
-    <li class="nav-item">
-      <a class="nav-link" href="">ログイン</a>
-    </li>
+    @guest
+      <li class="nav-item">
+        <a class="nav-link" href="">ログイン</a>
+      </li>
+    @endguest
 
-    <li class="nav-item">
-      <a class="nav-link" href=""><i class="fas fa-pen mr-1"></i>投稿する</a>
-    </li>
+    @auth
+      <li class="nav-item">
+        <a class="nav-link" href=""><i class="fas fa-pen mr-1"></i>投稿する</a>
+      </li>
+    @endauth
 
-    <!-- Dropdown -->
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
-          aria-haspopup="true" aria-expanded="false">
-        <i class="fas fa-user-circle"></i>
-      </a>
-      <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-        <button class="dropdown-item" type="button"
-                onclick="location.href=''">
-          マイページ
-        </button>
-        <div class="dropdown-divider"></div>
-        <button form="logout-button" class="dropdown-item" type="submit">
-          ログアウト
-        </button>
-      </div>
-    </li>
-    <form id="logout-button" method="POST" action="">
-    </form>
-    <!-- Dropdown -->
+    @auth
+      <!-- Dropdown -->
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-user-circle"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+          <button class="dropdown-item" type="button"
+                  onclick="location.href=''">
+            マイページ
+          </button>
+          <div class="dropdown-divider"></div>
+          <button form="logout-button" class="dropdown-item" type="submit">
+            ログアウト
+          </button>
+        </div>
+      </li>
+      <form id="logout-button" method="POST" action="{{ route('logout') }}">
+        @csrf
+      </form>
+      <!-- Dropdown -->
+    @endauth
 
   </ul>
 
