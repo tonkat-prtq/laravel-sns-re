e9cffcf57f24249af8004dc371aaf0b6e83f0d44
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-09-10 21:41:22 +0900

    Add: ユーザー登録ページを追加

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,52 @@
+@extends('app')
+
+@section('title', 'ユーザー登録')
+
+@section('content')
+  <div class="container">
+    <div class="row">
+      <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+        <h1 class="text-center"><a class="text-dark" href="/">memo</a></h1>
+        <div class="card mt-3">
+          <div class="card-body text-center">
+            <h2 class="h3 card-title text-center mt-2">ユーザー登録</h2>
+
+            <div class="card-text">
+              {{-- route('register')で、名前付きルートのURLを返せる --}}
+              {{-- php artisan route:listで表示された表の、Name列にあるルートのことを名前付きルートという --}}
+              <form method="POST" action="{{ route('register') }}">
+                {{-- CSRF対策 --}}
+                @csrf
+                <div class="md-form">
+                  <label for="name">ユーザー名</label>
+                  {{-- old('name')で、直前のリクエストのパラメータを返す --}}
+                  {{-- これにより、入力した内容が保持された状態でユーザー登録画面が表示される --}}
+                  <input class="form-control" type="text" id="name" name="name" required value="{{ old('name') }}">
+                  <small>英数字3〜16文字</small>
+                </div>
+                <div class="md-form">
+                  <label for="email">メールアドレス</label>
+                  <input class="form-control" type="text" id="email" name="email" required value="{{ old('email') }}">
+                </div>
+                <div class="md-form">
+                  <label for="password">パスワード</label>
+                  <input class="form-control" type="password" id="password" name="password" required>
+                </div>
+                <div class="md-form">
+                  <label for="password_confirmation">パスワード(確認)</label>
+                  <input class="form-control" type="password" id="password_confirmation" name="password_confirmation" required>
+                </div>
+                <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ユーザー登録</button>
+              </form>
+
+              <div class="mt-0">
+                <a href="{{ route('login') }}" class="card-text">ログインはこちら</a>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
