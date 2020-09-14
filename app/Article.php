40fcd2e264ef88f30a120d5ed2abfe50d0ccb698
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-09-14 22:00:35 +0900

    Add: fillableの使用

--- app/Article.php
+++ app/Article.php
@@ -13,6 +13,13 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
 {
+    // strong parameter的な感じ
+    // 前持って許可するプロパティを指定しておくことで、不正なリクエストを弾く
+    protected $fillable = [
+        'title',
+        'body',
+    ];
+
     // ArticleとUserは多(Articles)対1(user)
     // :BelongsToで、userメソッドの戻り値の型宣言
     // BelongsToクラス以外の型が帰ってきた場合はTypeErrorという例外が発生し処理が終了する
