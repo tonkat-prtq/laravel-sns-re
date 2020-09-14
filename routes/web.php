54e5fef0ac428267934c8c76413e4f745087ecd5
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-09-14 21:04:56 +0900

    Add: resourceでArticleのルーティング一斉追加

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,6 @@ Route::get('/', 'ArticleController@index');
 // ('ArticleController@index') <- 第二引数には、どのコントローラで何のメソッドを実行するのかを文字列で渡す
 // ArticleControllerがコントローラの名前
 // indexがアクションメソッド名(xRailsで言うアクション名)
+
+// Railsでいう resources :articles
+Route::resource('/articles', 'ArticleController');
