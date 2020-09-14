37c9edaf353390f1797d03171e5892f268efa5e9
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-09-14 21:41:52 +0900

    Add: バリデーションとバリデーションエラー用の項目名追加

---
+++ app/Http/Requests/ArticleRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules() // バリデーションのルールを設定
+    {
+        return [
+            'titile' => 'required|max:55', // ['required', 'max:50']でも可
+            'body' => 'required|Max:500',
+        ];
+    }
+
+    public function attributes() // バリデーションエラーメッセージに表示させる項目名のカスタマイズ
+    {
+        return [
+            'title' => 'タイトル',
+            'body' => '本文',
+        ];
+    }
+}
