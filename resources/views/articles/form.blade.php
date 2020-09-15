d5306700136a3809c7c3b4255ad2595c7b2ddf41
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-09-15 20:50:51 +0900

    Add: formのvalueの値を、記事が存在する場合はその記事の値を使用するように

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -1,9 +1,14 @@
 @csrf
 <div class="md-form">
   <label>タイトル</label>
-  <input type="text" name="title" class="form-control" required value="{{ old('title') }}">
+  <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}">
+  {{-- 式1 ?? 式2 という形 --}}
+  {{-- 式1がnullでない場合は、式1が結果となる --}}
+  {{-- 式1がnullである場合は、式2が結果となる --}}
+  {{-- $article->titleがnullでない場合、$artile->titleが結果 --}}
+  {{-- $article->titleがnullの場合、old('title')が結果 --}}
 </div>
 <div class="form-group">
   <label></label>
-  <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ old('body') }}</textarea>
+  <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
 </div>
