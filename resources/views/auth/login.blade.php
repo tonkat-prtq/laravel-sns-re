3db36f236f52a547dd2a113ac6bc4e3232a3c6f0
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-09-16 21:28:49 +0900

    Add: loginページにパスワード忘れた人向けのリンク追加

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,6 +31,10 @@
                 {{-- 次回から自動でログインするを実現するためのもの --}}
                 <input type="hidden" name="remember" id="remember" value="on">
 
+                <div class="text-left">
+                  <a href="{{ route('password.request') }}" class="card-text">パスワードを忘れた方</a>
+                </div>
+
                 <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ログイン</button>
 
               </form>
