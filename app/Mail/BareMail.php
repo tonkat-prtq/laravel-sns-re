b9450bcb3c960a80a4e2de90cf3848f2b9748dae
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-09-17 20:55:40 +0900

    Fix: スペルミス修正

--- app/Mail/BareMail.php
+++ app/Mail/BareMail.php
@@ -30,6 +30,7 @@ class BareMail extends Mailable
     // buildメソッドは、メソッド内にメールの宛先や件名、使用するテンプレート(Blade)などを設定するコードを追加した上で自分自身を返す
     // 今回作成したBareMailクラスでは、メールの種類ごとの細かい設定は持たせず、その名の通り「空っぽ」の設定のメールとして使用
     // そのため、buildメソッドでは何も設定せず、そのまま自分自身を返すようにしてある
+    public function build()
     {
         return $this;
     }
