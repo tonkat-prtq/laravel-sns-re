3113f4d58c0e05a24de3a9cfbf1e51bec94f97db
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-09-23 10:55:33 +0900

    Add: Laravel Mixを使ってVueを各Bladeで使用できるように

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,9 +17,13 @@
 </head>
 
 <body>
+  {{-- id="app"を指定することで、resources/js/appに定義したVueコンポーネントを各Bladeで使用できるようになる --}}
+  <div id="app">
+    @yield('content')
+  </div>
 
-  @yield('content')
-
+  {{-- Bladeでmix関数にJavaScriptファイルのパスを渡すと、その後ろにidパラメーターを付与してくれる --}}
+  <script src="{{ mix('js/app.js') }}"></script>
   <!-- JQuery -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <!-- Bootstrap tooltips -->
