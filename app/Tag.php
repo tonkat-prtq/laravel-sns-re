db0e583696c4b8ceb74707551198477923ecf6a0
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-09-25 08:02:57 +0900

    Add: fillableでTagのnameの保存を許可

--- app/Tag.php
+++ app/Tag.php
@@ -5,6 +5,10 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
-{
-    //
+{   
+    // strong parameter的なもの
+    // ここに記述されていないプロパティは、firstOrCreateやcreateメソッドでモデルが保存できない
+    protected $fillable = [
+        'name',
+    ];
 }
