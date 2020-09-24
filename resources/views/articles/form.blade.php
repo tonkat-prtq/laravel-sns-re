64a56e2e9510992e38e418bb5e69d96b29a72bfb
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-09-25 06:46:49 +0900

    Add: Articleのform用viewに、tags-inputのVueコンポーネントを読み込む

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -9,6 +9,11 @@
   {{-- $article->titleがnullの場合、old('title')が結果 --}}
 </div>
 <div class="form-group">
+  <article-tags-input
+  >
+  </article-tags-input>
+</div>
+<div class="form-group">
   <label></label>
   <textarea name="body" required class="form-control" rows="16" placeholder="本文">{{ $article->body ?? old('body') }}</textarea>
 </div>
