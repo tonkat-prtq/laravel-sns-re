b6fdd64ca95464df16375b50686e0cb9c0e52ec8
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-09-29 21:17:32 +0900

    Fix: スペルミス

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,10 +1,11 @@
 <?php
-
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
@@ -42,5 +43,25 @@ class LoginController extends Controller
     public function redirectToProvider(string $provider)
     {
         return Socialite::driver($provider)->redirect();
+    }
+
+    public function handleProviderCallback(Request $request, string $provider)
+    {
+        $providerUser = Socialite::driver($provider)->stateless()->user();
+        // Laravel\Socialite\Two\Userというクラスのインスタンスを取得できる
+
+        $user = User::where('email', $providerUser->getEmail())->first();
+        // Googleから取得したユーザー情報から、メールアドレスを取り出し、usersテーブルに存在するかを調べている
+        // $providerUser->getEmail()で、Googleから取得したユーザー情報からメールアドレスを取得
+
+        if($user) {
+        // $userがnullでなければ(Googleから取得したメールアドレスを同じメールアドレスを持つユーザーモデルが存在すれば)以下の処理を行う
+
+            $this->guard()->login($user, true);
+            // userをログイン状態にする
+
+            return $this->sendLoginResponse($request);
+            // ログイン後の画面（記事一覧画面）に遷移
+        }
     }
 }
